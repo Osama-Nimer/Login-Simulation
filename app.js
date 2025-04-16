a1bff59eb94ccec30a7e05507f73d83cdf69f650
Osama-Nimer <osamafares23@yahoo.com>
2025-04-16 20:26:22 +0300

    Some Updates !

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 import express from  "express";
 import bcrypt from  "bcrypt";
-
+import jwt from "jsonwebtoken";
 const app = express();
-const port = 3000;
+import dotenv from 'dotenv';
+const port = process.env.PORT || 3000;
 
 const users = [];
 app.use(express.json());
-
+dotenv.config();
 app.post("/register",async (req,res) =>{
     try {
         const { email , password} = req.body;
+        
+        /* Start Input Validation ...*/
+
+
+        /* End   Input Validation ...*/
+        
         const find = users.find((data) => email == data.email);
+        
         if(find)
             res.status(400).send("invalid email or password");
-        const hashedPass = await bcrypt.hash(password,10).then();
+        const hashedPass = await bcrypt.hash(password,10);
         users.push({email: email , password: hashedPass});
-        res.status(201).send("Register Created Successfully");
+        const lastIndex = users[users.length-1];
+        const token = jwt.sign({ id : lastIndex.email },process.env.JWT_SECRET ,{
+        expiresIn : '24h',})
+        res.json({ token , message : "Regestration Successfully"})
     } catch (error) {
-        res.status(500).send(error);
-    }
+        console.log("JWT_SECRET:", process.env.JWT_SECRET);
+        res.status(500).send(error.message);
+    }   
 })
 
 app.post("/login",async (req,res) => {
@@ -28,8 +40,11 @@ app.post("/login",async (req,res) => {
         if(!find)
             res.status(400).send("invalid email or password");
         const passMatch = await bcrypt.compare(password , find.password);
-        if(passMatch)
-            res.status(200).send("Loggdin Successfully");
+        if(passMatch){
+            const token = jwt.sign({ id : find.email },process.env.JWT_SECRET ,{
+                expiresIn : '24h',})
+                res.status(200).json({ token }).send("Loggdin Successfully");
+        }
         else
             res.status(400).send("invalid email or password");
     } catch (error) {
